478895d68cbd1fa987dc0695d2a1e77e6254c6de
Prasanna7599 <prasannakumar.bhalshankar@gmail.com>
2026-09-15 11:41:23 +0530

    fix yaml syntax error in runs-on line

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
 class Handler(BaseHTTPRequestHandler):
-	def do_GET(self):
-		self.send_response(200)
-		self.end_headers()
-		self.wfile.write(b"Hello from my own container!")
-HTTPServer(("0.0.0.0", 8000), Handler).server_forever()
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Hello from my own container! this is new comment added")
+
+HTTPServer(("0.0.0.0", 8000), Handler).serve_forever()
