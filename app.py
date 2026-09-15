429b96b23c4fcce8fd339363cf33574b6089bc81
Prasanna7599 <prasannakumar.bhalshankar@gmail.com>
2026-09-15 12:03:52 +0530

    New change in app.py

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b"Hello from my own container! this is new comment added")
+        self.wfile.write(b"Hello from my own container! this is new comment added for docker image push")
 
 HTTPServer(("0.0.0.0", 8000), Handler).serve_forever()
